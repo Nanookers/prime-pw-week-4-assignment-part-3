b6c46dfecc0281eb96e8d876897b93f5de8cf107
Connor Clark <connordeclark@gmail.com>
2022-11-17 18:38:15 -0600

    Working on Empty

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,31 +2,31 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+console.log(basket);
 
 function addItem( food ){
-    console.log(`Adding item:`, food);
     basket.push( food );
+    console.log(`Adding item:`, food);
     return true;
 }
 console.log(addItem(`Kimchi`));
 console.log(addItem(`Bean Sprouts`));
 console.log(`Basket is:  ${basket}`);
 
-/*
-    for( let i=0; i<basket.length; i++){
-        food = basket[i++];
-        console.log(basket);
+
+function listItems( index ) {
+    for( let index of basket){
+        console.log(index);
     }
-    return food;
-}
-*/
-function listItems( food ) {
-    let item = food;
-    for( let item of basket){
-        console.log(item);
-    }
-    return basket;
+    return index;
 }
 console.log(listItems(basket));
 console.log(addItem(`Curry Paste`));
 console.log(listItems(basket));
+
+function empty(index) {
+    index = []; 
+    console.log(`the cart has no items:`, index);
+    return index;
+}
+console.log(empty(basket)); 
